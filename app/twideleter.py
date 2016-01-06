68f9c1ad599bfdb953748529ca8edb5f609b2cf4
lawliet3110 <lawel3110@gmail.com>
2016-01-06 16:37:46 +0900

    全体のリファクタリングをした

--- app/twideleter.py
+++ app/twideleter.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 import twitter
@@ -7,33 +7,40 @@ import time
 from datetime import datetime
 from datetime import timedelta
 
-# const
-api = twitter.Api(
-    consumer_key = conf.consumer_key,
-    consumer_secret = conf.consumer_secret,
-    access_token_key = conf.access_token_key,
-    access_token_secret = conf.access_token_secret,
-)
-
-t = datetime.now() + timedelta(weeks=-4)
-ti = int(time.mktime(t.timetuple()))
-max_id = -1
-while True:
-    status = twitter.Status
-    if max_id == -1:
-        status = api.GetUserTimeline(count=200)
-    else:
-        status = api.GetUserTimeline(count=200, max_id=max_id)
-        pass
-    status.reverse()
-    if len(status) == 0:
-        break
-    for s in status:
-        if ti < s.created_at_in_seconds:
-            max_id = s.id - 1
-            print("max_id = " + str(max_id) + " at " + s.created_at)
+def delete_tweets(api):
+    period = int(time.mktime((datetime.now() + timedelta(weeks=-4)).timetuple()))
+    max_id = -1
+    while True:
+        status = twitter.Status
+        if max_id == -1:
+            status = api.GetUserTimeline(count=200)
+        else:
+            status = api.GetUserTimeline(count=200, max_id=max_id)
+            pass
+        status.reverse()
+        if len(status) == 0:
             break
-        api.DestroyStatus(s.id)
-        print("destroied tweet: " + s.text + " at " + s.created_at)
+        for s in status:
+            if period < s.created_at_in_seconds:
+                max_id = s.id - 1
+                break
+            api.DestroyStatus(s.id)
+            print('destroied tweet: %s at %s' % (s.text, s.created_at))
+            pass
         pass
+    pass
+
+def main():
+    api = twitter.Api(
+        consumer_key = conf.consumer_key,
+        consumer_secret = conf.consumer_secret,
+        access_token_key = conf.access_token_key,
+        access_token_secret = conf.access_token_secret,
+    )
+
+    delete_tweets(api)
+    pass
+
+if __name__ == '__main__':
+    main()
     pass
